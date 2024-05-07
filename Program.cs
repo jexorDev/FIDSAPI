0b344573c77ef8d28dcd24552c8aab01e9c9699f
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2024-05-07 11:28:19 -0500

    add cors

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
-
+const string CorsPolicyName = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CorsPolicyName,
+    policy =>
+    {
+        policy.WithOrigins("https://bnafids.netlify.com");
+    });
+});
 
 var app = builder.Build();
 
@@ -14,7 +23,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
